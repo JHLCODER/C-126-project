2eb4199aea9b611857c5b74370369b0e6a5a4829
JHLCODER <79390831+JHLCODER@users.noreply.github.com>
2022-10-16 12:12:07 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
+song = "";
+song1 = "";
+leftWristX = "";
+leftWristY ="";
+rightWristX = "";
+rightWristY = "";
+
+function preload(){
+    song = loadSound("music.mp3");
+    song1 = loadSound("music2.mp3");
+   }
+
 function setup(){
-    canvas = createCanvas(550,550);
-    canvas.position(560, 150);
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+ function modelLoaded(){
+    console.log('PoseNet is Initialized');
+ }
+
+ function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX = "+ leftWristX+"LeftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWristX = "+ rightWristX+"RightWristY = "+ rightWristY);
+ }
 }
